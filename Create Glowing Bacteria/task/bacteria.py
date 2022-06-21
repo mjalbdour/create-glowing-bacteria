75950de25f4c7abf7754281c348599179d3c812c
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-21 06:20:24 +0300

    Stage 1/6: A complementary strand

--- Create Glowing Bacteria/task/bacteria.py
+++ Create Glowing Bacteria/task/bacteria.py
@@ -1,1 +1,22 @@
 # write your program here
+
+nitrogenous_bases = {
+    "A": "T",
+    "C": "G",
+    "T": "A",
+    "G": "C"
+}
+
+strand = input()
+# strand = "GACGTCTGTGCAAGTACTACTGTTCTGCAGTCACTTGAATTCGATACCCAGCTGTGTGCACTACCTCCTT"
+
+
+def create_complementary_strand(s):
+    c = ""
+    for i in range(len(s)):
+        c += nitrogenous_bases[s[i]]
+    return c
+
+
+complementary_strand = create_complementary_strand(strand)
+print(complementary_strand)
